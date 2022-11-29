dcb2112ad869eb59d556fc3d9f3e486fec9cc50f
sbhongade598@gmail.com <sbhongade598@gmail.com>
2022-11-29 13:36:12 +0900

    intial commit

---
+++ src/components/NewsFeed.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>this is home to all!!</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import NewsFeed from '../components/NewsFeed.vue'
+
+</script>
+
+<template>
+  <main>
+    <NewsFeed />
+  </main>
+</template>
